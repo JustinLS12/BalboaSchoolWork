a1478c2753ac762542c1a1e82dd9071d14cddb0a
JustinLS12 <40769364+JustinLS12@users.noreply.github.com>
2018-10-28 13:33:42 -0700

    mm

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+    
+	public static int pow(int base, int exponent){
+		for(int i = 1; i <= exponent; i++);{
+			
+		}
+	}
+}
